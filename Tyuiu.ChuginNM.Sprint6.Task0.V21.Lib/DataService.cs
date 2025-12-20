00918708281e2e32104fb725171e262dc51dc81a
CDOOR <CDOOR@CHUGIN>
2025-12-20 16:46:57 +0500

    Task0

--- Tyuiu.ChuginNM.Sprint6.Task0.V21.Lib/DataService.cs
+++ Tyuiu.ChuginNM.Sprint6.Task0.V21.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ChuginNM.Sprint6.Task0.V21.Lib
     {
         public double Calculate(int x)
         {
-            double res = Math.Round((2*Math.Pow(x, 3) + 1/2 * Math.Pow(x, 2) - 3.5 * x + 2), 3);
+            double res = Math.Round(2 * Math.Pow(x, 3) + 0.5 * Math.Pow(x, 2) - 3.5 * x + 2, 3);
             return res;
         }
     }
