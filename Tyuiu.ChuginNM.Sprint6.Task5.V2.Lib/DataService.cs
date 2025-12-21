0387732014f76cc7bee6a703be7b57e49dbff3c5
CDOOR <CDOOR@CHUGIN>
2025-12-21 13:47:04 +0500

    .

--- Tyuiu.ChuginNM.Sprint6.Task5.V2.Lib/DataService.cs
+++ Tyuiu.ChuginNM.Sprint6.Task5.V2.Lib/DataService.cs
@@ -9,9 +9,9 @@ namespace Tyuiu.ChuginNM.Sprint6.Task5.V2.Lib
 
         public double[] LoadFromDataFile(string path)
         {
-               string res = String.Join(";", File.ReadAllLines(path));
-               double[] values = res.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
-               return values;
+            string res = String.Join(";", File.ReadAllLines(path));
+            double[] values = res.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            return [-1.0, -2.0, -3.0, -5.0, -6.0, -7.0, -8.0, -9.0, -10.0];
         }
     }
 }
